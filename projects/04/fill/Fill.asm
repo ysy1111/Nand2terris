0623512ea426f39cb370ceec15c8e047fa311336
shuyy <ysy9498@gmail.com>
2021-05-18 00:39:56 -0700

    project 3&4 finished

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,74 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
+
 // Put your code here.
+@8192
+D=A
+@n
+M=D // number of RAMs of the whole screen 
+
+(KBDLoop)
+@KBD
+D = M
+@WhiteLoop
+D; JEQ // if no key is pressed, do nothing and keep detecting
+
+@SCREEN
+D=A
+@addr
+M=D // record the base address into addr
+
+@i
+M=0 // count the block of pixels. there are 256*512/16 = 8192 in total.
+
+(BlackInnerLoop)
+@n
+D=M
+@i
+D = D-M
+@KBDLoop
+D;JEQ // if i == n(8192), finish black the screen, go back to detect the keyboard
+
+
+@addr
+A=M
+M=-1
+
+@i
+M = M + 1
+@addr
+M = M + 1
+
+@BlackInnerLoop
+0;JMP
+
+(WhiteLoop)
+@SCREEN
+D=A
+@addr
+M=D // record the base address into addr
+
+@i
+M=0 // count the block of pixels. there are 256*512/16 = 8192 in total.
+
+(WhiteInnerLoop)
+@n
+D=M
+@i
+D = D-M
+@KBDLoop
+D;JEQ // if i == n(8196), finish black the screen, go back to detect the keyboard
+
+
+@addr
+A=M
+M=0
+
+@i
+M = M + 1
+@addr
+M = M + 1
+
+@WhiteInnerLoop
+0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,35 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@i
+M = 0 // create a counter named i
+
+@sum
+M = 0 //create a variable sum, store the summation
+
+(Loop)
+@i
+D = M
+@R1
+D = D - M
+@Endloop
+D;JEQ //if i == R1, end the loop
+
+@R0
+D = M 
+@sum
+M = D + M //sum = sum + R0
+@i
+M = M + 1
+@Loop
+0;JMP
+
+(Endloop)
+@sum
+D = M
+@R2
+M = D
+
+(End)
+@End
+0;JMP
